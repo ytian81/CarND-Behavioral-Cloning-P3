672c81a003e6db20d025b2f2125bbcdf0ee52395
Yang Tian <ytian.gt@gmail.com>
2018-12-27 20:30:43 -0800

    change to generator

--- model.py
+++ model.py
@@ -2,7 +2,9 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.layers import Activation, Conv2D, Cropping2D, Dense, Dropout, Flatten, Lambda, MaxPool2D
 from keras.models import Sequential
 from keras.regularizers import l2
+from math import ceil
 from scipy import ndimage
+from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
 import csv
@@ -10,40 +12,55 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def get_data():
-    # Read driving log data from csv file
-    lines = []
+    # Read driving log data from csv file, augment data by lazy flipping and using left and right images
+    samples = []
+    delta = 0.2
+    angle_corrections = [0.0, delta, -delta]
     with open('./data/driving_log.csv') as f:
         reader = csv.reader(f)
         for line in reader:
-            lines.append(line)
+            # Use center, left and right images
+            for idx in range(3):
+                # Original image
+                sample = [line[idx], (float(line[3])+angle_corrections[idx]), False]
+                samples.append(sample)
+                # Flipped image
+                sample = [line[idx], -(float(line[3])+angle_corrections[idx]), True]
+                samples.append(sample)
+    return samples
 
-    # Modify image path and extract outputs
-    images = []
-    steering_angles = []
-    delta = 0.2
-    for line in lines:
-        # Use center, left and right images
-        angle_corrections = [0.0, delta, -delta]
-        for idx in range(3):
-            image_path = line[idx]
-            image_path = './data/IMG/' + image_path.split('/')[-1]
-            image = ndimage.imread(image_path)
-            images.append(image)
-            steering_angle = float(line[3]) + angle_corrections[idx]
-            steering_angles.append(steering_angle)
+# Shuffle and split training/validation data
+validation_split = 0.3
+train_samples, validation_samples = train_test_split(get_data(), test_size = validation_split,
+        shuffle=True)
 
-            # Augment data (double the amount of data)
-            images.append(np.fliplr(image))
-            steering_angles.append(-steering_angle)
+shuffle_before_epoch = True
+def data_generator(samples, batch_size):
+    # Modify image path and extract images. Flip image if requested
+    num_samples = len(samples)
 
-    images = np.array(images)
-    steering_angles = np.array(steering_angles)
+    while True:
+        if shuffle_before_epoch:
+            shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
 
-    # shuffle data before split validation set
-    images, steering_angles = shuffle(images, steering_angles)
-    return images, steering_angles
+            images = []
+            steering_angles = []
+            for sample in batch_samples:
+                image_path = './data/IMG/' + sample[0].split('/')[-1]
+                image = ndimage.imread(image_path)
+                # Flip image
+                if (sample[2]):
+                    image = np.fliplr(image)
+                images.append(image)
+                steering_angles.append(sample[1])
 
-X_train, y_train = get_data()
+            yield np.array(images), np.array(steering_angles)
+
+batch_size = 32
+train_generator = data_generator(train_samples, batch_size)
+validation_generator = data_generator(validation_samples, batch_size)
 
 def assemble_model():
     # Assemble model
@@ -91,8 +108,13 @@ model.compile(loss='mse', optimizer='adam')
 # Train 15 epoches at most and save the best model, early stop if validation loss stops improving
 checkpoint = ModelCheckpoint('model.h5', monitor='val_loss', save_best_only=True, mode='min', verbose=0)
 earlystop = EarlyStopping(monitor='val_loss', patience=3, mode='min')
-history_object = model.fit(X_train, y_train, validation_split=0.3, shuffle=True, epochs=15,
-        callbacks=[checkpoint, earlystop])
+history_object = model.fit_generator(train_generator,
+        steps_per_epoch=ceil(len(train_samples)/batch_size),
+        validation_data=validation_generator,
+        validation_steps=ceil(len(validation_samples)/batch_size),
+        epochs=15,
+        callbacks=[checkpoint, earlystop],
+        verbose=1)
 
 # Draw training statistics
 plt.plot(history_object.history['loss'])
